b3f3af437cae2db2298abe5e6b8899588232e96a
akbiggs <alexanderkbiggs@gmail.com>
2013-09-21 15:27:06 -0400

    Add some basic point help methods.

---
+++ bombmanplayer/PointHelper.java
@@ -0,0 +1,26 @@
+import java.awt.Point;
+
+
+public class PointHelper {
+	private PointHelper() {
+		
+	}
+	
+	public static Point add(Point p1, Point p2) {
+		return new Point(p1.x + p2.x, p1.y + p2.y);
+	}
+	
+	
+	public static Point sub(Point p1, Point p2) {
+		return new Point(p1.x - p2.x, p1.y - p2.y);
+	}
+
+	public static Point mul(Point p1, Point p2) {
+		return new Point(p1.x * p2.x, p1.y * p2.y);
+	}
+	
+	public static int manhattenDistance(Point p1, Point p2) {
+		return (Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y));
+		
+	}
+}
